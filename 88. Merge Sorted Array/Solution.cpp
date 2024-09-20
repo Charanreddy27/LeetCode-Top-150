c3b444e48eb2b40a7d8049270133ebb192ce2fa2
Marreddy Charan Bapi Reddy <charanbapireddy@gmail.com>
2024-09-20 08:44:39 +0530

    C++ Solution
            Submission Statistics:
            Question #: 88
            Language: C++
            Runtime: 5 ms
            Runtime percentile: 28.34
            Memory: 11.436 MB
            Memory percentile: 23.41

---
+++ 88. Merge Sorted Array/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> res;
+        int i=0,j=0,k=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                res.push_back(nums1[i]);
+                k++;i++;
+            }
+            else{
+                res.push_back(nums2[j]);
+                k++;j++;
+            }
+        }
+        while(i<m){
+            res.push_back(nums1[i]);
+            i++;k++;
+        }
+        while(j<n){
+            res.push_back(nums2[j]);
+            j++;k++;
+        }
+        nums1 = res;
+    }
+};
