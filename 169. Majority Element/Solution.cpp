0fbadac9cafb25baecbeed2d0d73d36a05a7345a
Marreddy Charan Bapi Reddy <charanbapireddy@gmail.com>
2024-09-20 09:03:35 +0530

    C++ Solution
            Submission Statistics:
            Question #: 169
            Language: C++
            Runtime: 23 ms
            Runtime percentile: 21.30
            Memory: 28.212 MB
            Memory percentile: 6.44

---
+++ 169. Majority Element/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        for(auto c:mp){
+            if(c.second > nums.size()/2) return c.first;
+        }
+        return -1;
+    }
+};
