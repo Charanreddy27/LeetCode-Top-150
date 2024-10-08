237e96ea21c4da15e8f8a21998de224999f29145
Marreddy Charan Bapi Reddy <charanbapireddy@gmail.com>
2024-10-08 06:05:09 +0530

    C++ Solution
            Submission Statistics:
            Question #: 2095
            Language: C++
            Runtime: 116 ms
            Runtime percentile: 39.11
            Memory: 35.864 MB
            Memory percentile: 33.36

---
+++ 2095. Minimum Number of Swaps to Make the String Balanced/Solution.cpp
@@ -0,0 +1,26 @@
+
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        stack<char> st;
+        for(auto&c : s){
+            if(c == '['){
+                // Push opening brackets onto the stack
+                st.push(c);
+            }else{ // c == ']'
+                if(!st.empty() && st.top() == '[') {
+                    // If there's a matching opening bracket on the stack, pop it
+                    st.pop();
+                }else{
+                    // Otherwise, push the closing bracket onto the stack
+                    st.push(c);
+                }
+            }
+        }
+        // The stack now contains unmatched brackets
+        // Half of the stack size (rounded up) will be the number of swaps needed
+        int unmatched = st.size();
+        return (unmatched / 2 + 1) / 2;
+    }
+};
